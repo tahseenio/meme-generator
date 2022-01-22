379851dd9cba3a38fb90b0e2ab6cd4fdfacd6d1a
Tahseen <>
2022-01-22 15:40:15 +1000

    first deploy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import MemeGenerator from './components/MemeGenerator'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+
+    )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <img src="https://www.pngall.com/wp-content/uploads/2016/05/Trollface-PNG.png" alt="" />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
