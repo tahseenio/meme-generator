52a6508f14f29498ba2c8ed9a1a7beac5fcf9d0f
Tahseen <tahseenislam@hotmail.com>
2022-05-06 23:33:12 +1000

    converted to tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img
+        src='https://www.pngall.com/wp-content/uploads/2016/05/Trollface-PNG.png'
+        alt=''
+      />
+      <p>Meme Generator</p>
+    </header>
+  );
+}
+
+export default Header;
